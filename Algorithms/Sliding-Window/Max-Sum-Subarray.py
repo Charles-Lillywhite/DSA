8ad6710035788ef3bc3068a78d6f221001a80974
Charles-Lillywhite <115148482+Charles-Lillywhite@users.noreply.github.com>
2022-11-21 11:38:38 +0000

    Update Max-Sum-Subarray.py

--- Algorithms/Sliding-Window/Max-Sum-Subarray.py
+++ Algorithms/Sliding-Window/Max-Sum-Subarray.py
@@ -1,5 +1,5 @@
 # Maximum sum of a contiguous subarray with size k
-# E.g.  A = [4,2,1,7,8,1,2,8,1,0], k = 3 would yield 16 from [7, 8, 1] (or [1, 8, 7])
+# E.g.  A = [4,2,1,7,8,1,2,8,1,0], k = 3 would yield 16 from [7, 8, 1] (or [1, 7, 8])
 
 def MaxSumSubarray(A, k):
     maxsum = - sys.maxsize
