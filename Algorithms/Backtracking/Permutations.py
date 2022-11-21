8cc820da6be9d3c859d1347c4e3e557cd193ab89
Charles-Lillywhite <115148482+Charles-Lillywhite@users.noreply.github.com>
2022-11-21 11:54:07 +0000

    Update Permutations.py

--- Algorithms/Backtracking/Permutations.py
+++ Algorithms/Backtracking/Permutations.py
@@ -1,3 +1,11 @@
+'''
+Generate all UNIQUE permutations of the elements in an array.
+
+E.g.
+
+permutations([1, 2, 3]) should return [ [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1] ]
+'''
+
 def permutations(nums):
     
     def search(state, solutions):
