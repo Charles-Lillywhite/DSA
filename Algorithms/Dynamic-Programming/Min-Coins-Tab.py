f17d49d262ab8a91a31e73c6b76d43fe35f88de3
Charles-Lillywhite <115148482+Charles-Lillywhite@users.noreply.github.com>
2022-11-21 19:36:25 +0000

    Create Min-Coins-Tab.py

---
+++ Algorithms/Dynamic-Programming/Min-Coins-Tab.py
@@ -0,0 +1,14 @@
+def coinChangeTab(coins, amount):
+  
+    tab = [amount+1] * (amount+1)
+    tab[0] = 0
+
+    for i in range(0, amount+1):
+        for c in coins:
+            if (i + c) <= amount:
+                tab[i+c] = min(tab[i+c], tab[i]+1)
+    
+    if tab[-1] == amount + 1:
+        return -1
+      
+    return tab[-1]
