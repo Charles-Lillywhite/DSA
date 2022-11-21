07ef975bc1dabd897bc8163755479cca02f35596
Charles-Lillywhite <115148482+Charles-Lillywhite@users.noreply.github.com>
2022-11-21 10:18:38 +0000

    Update Linked-List.py

--- Data-Structures/Linked-Lists/Linked-List.py
+++ Data-Structures/Linked-Lists/Linked-List.py
@@ -1,15 +1,4 @@
-'''
-In a standard array, item are stored sequentially in a contiguous block of memory.
-In a linked list this is not the case; each 'node' contains the value of
-the data item, as well as a pointer to the memory location of the next element.
 
-Linked lists are a simple data structure for storing a sequential collection.
-Unlike a standard Python list, it will allow us to insert at the beginning
-quickly. The idea is to store the items in individual objects called nodes.
-A special node is the head of the list (we may also denote the tail of the list, and include pointers in both directions).
-Each node stores a reference to the next node in the list.
-'''
-        
 class Node:
     def __init__(self, data, link = None):
         self.data = data
