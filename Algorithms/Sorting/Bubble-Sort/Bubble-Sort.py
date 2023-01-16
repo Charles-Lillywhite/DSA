630c5d05f1902c6d8b0d58d9e1661838446bdf37
Charles-Lillywhite <115148482+Charles-Lillywhite@users.noreply.github.com>
2023-01-16 10:56:26 +0000

    add early return once sorted

--- Algorithms/Sorting/Bubble-Sort/Bubble-Sort.py
+++ Algorithms/Sorting/Bubble-Sort/Bubble-Sort.py
@@ -1,7 +1,16 @@
 def bubbleSort(arr):
+    
     for iteration in range(0, len(arr) - 1):
+        
+        isSorted = True
+        
         for j in range(0, len(arr) - iteration - 1): 
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                isSorted = False
+        
+        if isSorted:
+            return arr
+      
 
     return arr
