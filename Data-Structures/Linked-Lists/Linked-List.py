0e34711b53ee0bba8b8ef3377ec4f78e81d7fe9f
Charles-Lillywhite <115148482+Charles-Lillywhite@users.noreply.github.com>
2023-01-17 12:06:03 +0000

    Update Linked-List.py

--- Data-Structures/Linked-Lists/Linked-List.py
+++ Data-Structures/Linked-Lists/Linked-List.py
@@ -1,49 +1,45 @@
-
 class Node:
-    def __init__(self, data, link = None):
-        self.data = data
+    def __init__(self, value, link = None):
+        self.val = value
         self.link = link
 
 class MyLinkedList:
 
     def __init__(self):
-        self.head = None
         self.length = 0
+        self.head = None
         
-
     def get(self, index: int) -> int:
-        if index < 0 or index >= self.length:
+        
+        if index < 0 or index > self.length - 1:
             return -1
-        else:
-            current = self.head
-            for _ in range(1, index+1):
-                current = current.link
-            return current.data
-
+        
+        current = self.head
+        for _ in range(1, index + 1):
+            current = current.link
+        return current.val
+    
     def addAtHead(self, val: int) -> None:
         self.head = Node(val, self.head)
         self.length += 1
-        
-        
+
     def addAtTail(self, val: int) -> None:
-        if self.head is None:
+        if not self.head:
             self.addAtHead(val)
         else:
             current = self.head
-
-            while current.link is not None:
+            while current.link:
                 current = current.link
             current.link = Node(val)
+
             self.length += 1
-        
+
     def addAtIndex(self, index: int, val: int) -> None:
         if index < 0 or index > self.length:
             return
         elif index == 0:
-            print('switch to addHead')
             self.addAtHead(val)
         elif index == self.length:
-            print('switch to addTail')
             self.addAtTail(val)
         else:
             current = self.head
@@ -52,27 +48,25 @@ class MyLinkedList:
             current.link = Node(val, current.link)
             self.length += 1
             
+        
     def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >= self.length:
+        if index < 0 or index > self.length - 1:
             return
         elif index == 0:
             self.head = self.head.link
-            self.length -= 1
+            
         else:
             current = self.head
             for _ in range(1, index):
                 current = current.link
             current.link = current.link.link
-            self.length -= 1
         
-    
+        self.length -= 1
+        
+        
     def printList(self):
-        if self.head is None:
-            print('Empty')
-        else:
-            current = self.head
-            for _ in range(0, self.length):
-                print(f'{current.data} ->', end = ' ')
-                current = current.link
+        for idx in range(self.length):
+            currentVal = self.get(idx)
+            print(currentVal, end = ' - ')
         print('None')
         print('\n')
